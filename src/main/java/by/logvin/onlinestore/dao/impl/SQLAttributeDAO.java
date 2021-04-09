4e9affb881f53fb59caf6d9c1e5a492336cafc54
Vlad Logvin <vladlogvin97@gmail.com>
2021-04-09 21:01:17 +0300

    Attribute DAO body written

--- src/main/java/by/logvin/onlinestore/dao/impl/SQLAttributeDAO.java
+++ src/main/java/by/logvin/onlinestore/dao/impl/SQLAttributeDAO.java
@@ -1,13 +1,11 @@
 package by.logvin.onlinestore.dao.impl;
 
 import by.logvin.onlinestore.bean.Attribute;
-import by.logvin.onlinestore.bean.Category;
 import by.logvin.onlinestore.dao.AttributeDAO;
 import by.logvin.onlinestore.dao.connection.ConnectionPool;
 import by.logvin.onlinestore.dao.connection.ConnectionPoolException;
 import by.logvin.onlinestore.dao.exception.DAOException;
 import by.logvin.onlinestore.dao.impl.sqlrequest.AttributeSQLRequest;
-import by.logvin.onlinestore.dao.impl.sqlrequest.CategorySQLRequest;
 import org.apache.log4j.Logger;
 
 import java.sql.Connection;
