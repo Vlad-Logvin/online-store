1164c29b5d9cb476e28ef173d706767d6ed1c327
Vlad Logvin <vladlogvin97@gmail.com>
2021-04-09 20:05:32 +0300

    Category DAO body written

--- src/main/java/by/logvin/onlinestore/dao/impl/SQLCategoryDAO.java
+++ src/main/java/by/logvin/onlinestore/dao/impl/SQLCategoryDAO.java
@@ -2,22 +2,132 @@ package by.logvin.onlinestore.dao.impl;
 
 import by.logvin.onlinestore.bean.Category;
 import by.logvin.onlinestore.dao.CategoryDAO;
+import by.logvin.onlinestore.dao.connection.ConnectionPool;
+import by.logvin.onlinestore.dao.connection.ConnectionPoolException;
 import by.logvin.onlinestore.dao.exception.DAOException;
+import by.logvin.onlinestore.dao.impl.sqlrequest.CategorySQLRequest;
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class SQLCategoryDAO implements CategoryDAO {
 
+    private final static Logger logger = Logger.getLogger(SQLCategoryDAO.class);
+
     @Override
     public boolean addCategory(String name) throws DAOException {
-        return false;
+        Connection connection = getConnection();
+        logger.info("Connection established");
+        PreparedStatement preparedStatement = null;
+        int numberOfUpdateLines = 0;
+        try {
+            preparedStatement = connection.prepareStatement(CategorySQLRequest.insertCategory);
+            preparedStatement.setString(1, name);
+            numberOfUpdateLines = preparedStatement.executeUpdate();
+            logger.info("Request (" + preparedStatement.toString() + ") was completed");
+        } catch (SQLException e) {
+            logger.error("SQLException was thrown due to an error during prepared statement creation or execution", e);
+            throw new DAOException("Error prepared statement updating or setting data", e);
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                logger.error("Prepared statement has been already closed", e);
+            }
+        }
+
+        removeConnection(connection);
+        logger.info("Connection is broken");
+
+        return numberOfUpdateLines != 0;
     }
 
     @Override
     public boolean deleteCategory(int categoryID) throws DAOException {
-        return false;
+        Connection connection = getConnection();
+        logger.info("Connection established");
+        PreparedStatement preparedStatement = null;
+        int numberOfUpdateLines = 0;
+        try {
+            preparedStatement = connection.prepareStatement(CategorySQLRequest.deleteCategory);
+            preparedStatement.setInt(1, categoryID);
+            numberOfUpdateLines = preparedStatement.executeUpdate();
+            logger.info("Request (" + preparedStatement.toString() + ") was completed");
+        } catch (SQLException e) {
+            logger.error("SQLException was thrown due to an error during prepared statement creation or execution", e);
+            throw new DAOException("Error prepared statement updating or setting data", e);
+        }finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                logger.error("Prepared statement has been already closed", e);
+            }
+        }
+
+        removeConnection(connection);
+        logger.info("Connection is broken");
+
+        return numberOfUpdateLines != 0;
     }
 
     @Override
     public Category getCategory(int categoryID) throws DAOException {
-        return null;
+        Connection connection = getConnection();
+        logger.info("Connection established");
+        PreparedStatement preparedStatement = null;
+        Category category = null;
+        ResultSet resultSet = null;
+        try {
+            preparedStatement = connection.prepareStatement(CategorySQLRequest.selectCategoryByID);
+            preparedStatement.setInt(1, categoryID);
+            resultSet = preparedStatement.executeQuery();
+            logger.info("Request (" + preparedStatement.toString() + ") was completed");
+            if (!resultSet.next()) {
+                logger.info("Category with this id was not found");
+                throw new DAOException("Error category id");
+            }
+            category = new Category(
+                    resultSet.getInt("c_id"),
+                    resultSet.getString("c_name")
+            );
+        } catch (SQLException e) {
+            logger.error("SQLException was thrown due to an error during prepared statement creation or execution", e);
+            throw new DAOException("Error prepared statement updating or setting data", e);
+        }finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                logger.error("Prepared statement has been already closed", e);
+            }
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                logger.error("Result set has been already closed", e);
+            }
+        }
+
+        removeConnection(connection);
+        logger.info("Connection is broken");
+
+        return category;
+    }
+
+    private Connection getConnection() throws DAOException {
+        Connection connection = null;
+        try {
+            connection = ConnectionPool.getInstance().getConnection();
+            logger.info("Connection is gotten from connection pool");
+        } catch (ConnectionPoolException e) {
+            logger.error("ConnectionPoolException was thrown due to an error during getting connection from connection pool", e);
+            throw new DAOException("Error with getting pool from server", e);
+        }
+        return connection;
+    }
+
+    private boolean removeConnection(Connection connection) {
+        return ConnectionPool.getInstance().removeConnection(connection);
     }
 }
--- src/main/java/by/logvin/onlinestore/dao/impl/sqlrequest/CategorySQLRequest.java
+++ src/main/java/by/logvin/onlinestore/dao/impl/sqlrequest/CategorySQLRequest.java
@@ -1,4 +1,18 @@
 package by.logvin.onlinestore.dao.impl.sqlrequest;
 
-public class CategorySQLRequest {
+public final class CategorySQLRequest {
+    public static final String insertCategory =
+            "INSERT INTO categories(c_name) " +
+                    "VALUES (?)";
+    public static final String deleteCategory =
+            "DELETE FROM categories c " +
+                    "WHERE c.c_id=?";
+    public static final String selectCategoryByID =
+            "SELECT * " +
+                    "FROM categories c " +
+                    "WHERE c.c_id=?";
+
+
+    private CategorySQLRequest() {
+    }
 }
