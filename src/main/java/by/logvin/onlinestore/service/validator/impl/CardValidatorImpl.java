df507398cdca41b9675be1044fc5f734c98e6de6
Vlad Logvin <vladlogvin97@gmail.com>
2021-04-11 02:32:46 +0300

    Card validator is written

--- src/main/java/by/logvin/onlinestore/service/validator/impl/CardValidatorImpl.java
+++ src/main/java/by/logvin/onlinestore/service/validator/impl/CardValidatorImpl.java
@@ -2,10 +2,36 @@ package by.logvin.onlinestore.service.validator.impl;
 
 import by.logvin.onlinestore.service.validator.CardValidator;
 
+import java.time.LocalDateTime;
+
 public class CardValidatorImpl implements CardValidator {
+    private final static String CARD_NUMBER_REGEX = "[0-9]{16}";
+    private final static String VALIDITY_PERIOD_REGEX = "((1[0-2])|([1-9]))[0-9]{2}";
+    private final static String AUTHENTICATION_CODE_REGEX = "[0-9]{3}";
+    private final static Integer MAX_ISSUED_CARD = 5;
 
     @Override
     public boolean validate(long number, int validityPeriod, int authenticationCode) {
-        return false;
+        boolean isValidValidityPeriod = Integer.toString(validityPeriod).matches(VALIDITY_PERIOD_REGEX);
+        if (isValidValidityPeriod) {
+            int month = validityPeriod / 100;
+            int year = validityPeriod - month * 100;
+            LocalDateTime now = LocalDateTime.now();
+            System.out.println(now);
+            if (now.getYear() == year) {
+                if (now.getMonthValue() - month < 0) {
+                    return false;
+                }
+            }
+            if (month > 12) {
+                return false;
+            }
+            if (year - MAX_ISSUED_CARD > now.getYear() % 100) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+        return Long.toString(number).matches(CARD_NUMBER_REGEX) && Integer.toString(authenticationCode).matches(AUTHENTICATION_CODE_REGEX);
     }
 }
